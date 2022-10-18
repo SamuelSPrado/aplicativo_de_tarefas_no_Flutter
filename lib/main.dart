65959d9d7dc3cbe4331aeed4dafa7e5ee160dc61
SamuelSPrado <samuelsantanaprado@gmail.com>
2022-10-18 04:54:04 -0300

    Implementando a classe Tarefas e alterando parâmetros do Stateless Widget para enviar argumentos e replicar os Containers

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,57 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.green,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter: Meus primeiros passos'),
+          title: Text('Tarefas'),
         ),
-        body: Container(),
-        floatingActionButton: FloatingActionButton(onPressed: (){}),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Tarefa('Ler'),
+            Tarefa('Estudar'),
+            Tarefa('Aprender Flutter'),
+            Tarefa('Jogar'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class Tarefa extends StatelessWidget {
+  final String nome;
+  const Tarefa(this.nome,{Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.green,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Text(nome),
+                ElevatedButton(onPressed: (){}, child: Icon(Icons.upgrade))
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
