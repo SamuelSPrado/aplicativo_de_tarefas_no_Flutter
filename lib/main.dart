22034e3a195e10854a4d65ba637576b4e69dd16b
SamuelSPrado <samuelsantanaprado@gmail.com>
2022-10-18 05:32:53 -0300

    alterando o estado da classe Tarefa para Stateful Widget e adicionando setState

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,13 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: ListView(
           children: [
             Tarefa('Ler'),
             Tarefa('Estudar'),
             Tarefa('Aprender Flutter'),
             Tarefa('Jogar'),
+            Tarefa('Lavar louça'),
           ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -33,36 +33,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Tarefa extends StatelessWidget {
+class Tarefa extends StatefulWidget {
   final String nome;
-  const Tarefa(this.nome,{Key? key}) : super(key: key);
+
+  const Tarefa(this.nome, {Key? key}) : super(key: key);
 
   @override
+  State<Tarefa> createState() => _TarefaState();
+}
+
+class _TarefaState extends State<Tarefa> {
+  int nivel = 0;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            color: Colors.green,
-            height: 140,
-          ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.green,
+              height: 140,
+            ),
+            Column(
               children: [
                 Container(
-                  color: Colors.black26,
-                  width: 72,
+                  color: Colors.white,
                   height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Container(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: TextStyle(fontSize: 24),
+                          )),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              nivel++;
+                            });
+                            print(nivel);
+                          },
+                          child: Icon(Icons.upgrade))
+                    ],
+                  ),
                 ),
-                Text(nome),
-                ElevatedButton(onPressed: (){}, child: Icon(Icons.upgrade))
+                Text(
+                  'Nível: $nivel',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
               ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
